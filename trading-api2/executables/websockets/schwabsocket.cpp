9fd3b89c0f47714640a538b4a7ecf50a3112d38e
William Liu <emoninjaaah@gmail.com>
2024-04-04 21:57:18 -0500

    Simple schwabsocket test executable to test uWebSocket connection to Schwab websocket stream

--- trading-api2/executables/websockets/schwabsocket.cpp
+++ trading-api2/executables/websockets/schwabsocket.cpp
@@ -2,7 +2,39 @@
 
 #include <iostream>
 
+std::string testlogin = R"({
+    "service": "ADMIN",
+    "requestid": 0,
+    "command": "LOGIN",
+    "SchwabClientCustomerId": "<streamerInfo[0].schwabClientCustomerId>",
+    "SchwabClientCorrelId": "<streamerInfo[0].schwabClientCorrelId>",
+    "parameters":
+    {
+        "Authorization": "<ACCESS_TOKEN>",
+        "SchwabClientChannel": "<streamerInfo[0].schwabClientChannel>",
+        "SchwabClientFunctionId": "<streamerInfo[0].schwabClientFunctionId>"
+    }
+})";
+
 int main(int argc, char** argv)
 {
-    streamer::IStreamHandler* handler = new streamer::SchwabStreamHandler("testurl");
+    uWS::Hub hub;
+    uWS::Group<uWS::CLIENT>* group;
+
+    group = hub.createGroup<uWS::CLIENT>();
+
+    group->onConnection([](uWS::WebSocket<uWS::CLIENT>* ws, uWS::HttpRequest)
+    {
+        std::cout << "test\n";
+        ws->send(testlogin.data(), testlogin.size(), uWS::OpCode::TEXT);
+    });
+
+    group->onMessage([](uWS::WebSocket<uWS::CLIENT>* ws, char* message, size_t length, uWS::OpCode opCode)
+    {
+        std::string text = std::string(message, length);
+        std::cout << text << "\n";
+    });
+
+    hub.connect("wss://streamer-api.schwab.com/ws", nullptr, {}, 5000, group);
+    hub.run();
 }
