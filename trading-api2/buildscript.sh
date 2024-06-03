56970d33aaafa26cc87296280381221fafa8f34f
William <emoninjaaah@gmail.com>
2024-06-03 04:37:58 +0000

    Remove BUILD_TESTS option - always build tests along with production executables - buildscript now takes in folder to copy all built files as second argument

--- trading-api2/buildscript.sh
+++ trading-api2/buildscript.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 REBUILD=$1
-BUILD_TESTS=$2
+COPYFILESTO=$2
 
 if [[ $REBUILD -gt 0 ]]
 then
     rm -rf output/
     mkdir output/
-    cmake -DBUILD_TESTS=$BUILD_TESTS -S . -B output/
+    cmake -S . -B output/
     cmake --build output/ -j 16
+    cp -rf output/* $COPYFILESTO
 else
     echo "normal build\n"
     cmake --build output/ -j 16
